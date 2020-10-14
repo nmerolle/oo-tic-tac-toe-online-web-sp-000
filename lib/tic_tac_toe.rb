a3be533fc309a076fb33851294d8eb7bfa141942
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-14 14:38:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-end 
+ 
 
 # The "pipes" || = or. This is saying that if the board returns nil, return an empty array.  So, either display the current board, or a new one.
 
@@ -149,6 +149,7 @@ end
 
 end
 
+end
 
 
 
